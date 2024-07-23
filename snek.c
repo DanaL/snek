8008da03848f958b081735eb19a5ab0ec3432b63
Dana Larose <ywg.dana@gmail.com>
2024-07-23 07:44:57 -0500

    Arrow keys

--- snek.c
+++ snek.c
@@ -283,8 +283,22 @@ char get_key(void)
   char c = '\0';
   if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
   
-  if (c == CTRL_KEY('q')) 
+  if (c == CTRL_KEY('q')) {
     exit(0);
+  }
+  else if (c == '\x1b') {
+    char seq[3];
+    if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+    if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+    if (seq[0] == '[') {
+      switch (seq[1]) {
+        case 'A': return 'w';
+        case 'B': return 's';
+        case 'C': return 'd';
+        case 'D': return 'a';
+      }
+    }
+  }
 
   return c;
 }
